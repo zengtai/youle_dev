4e6edf4284629d00b06852d54707a1bda682f0c0
zengtai <zengtai@gmail.com>
2022-07-19 16:52:43 +0800

    首页广告位样式更改

--- pages/index.js
+++ pages/index.js
@@ -17,28 +17,30 @@ export default function Home({ data, categories }) {
           .sort((a, b) => (a.total < b.total ? 1 : -1))
           .map((item, index) => {
             return (
-              <section key={item.category.slug}>
-                <header className="section-title my-3 mx-4 flex gap-2">
-                  <h1 className="font-bold">{`${item.category.name} Games`}</h1>
-                </header>
-                <ul className="mx-4 grid grid-cols-3 gap-4">
-                  {item.data.map((item) => (
-                    <ListItem
-                      item={item}
-                      key={item.id}
-                      lazy={index > 2 ? true : false}
-                    />
-                  ))}
-                </ul>
-                {item.total > 6 ? (
-                  <Link href={`/category/${item.category.slug}`}>
-                    <a>
-                      <div className="mx-4 flex justify-center border border-white/20 p-2 text-xs">
-                        <span className="uppercase">&nbsp;More</span>
-                      </div>
-                    </a>
-                  </Link>
-                ) : null}
+              <>
+                <section key={item.category.slug}>
+                  <header className="section-title my-3 mx-4 flex gap-2">
+                    <h1 className="font-bold">{`${item.category.name} Games`}</h1>
+                  </header>
+                  <ul className="mx-4 grid grid-cols-3 gap-4">
+                    {item.data.map((item) => (
+                      <ListItem
+                        item={item}
+                        key={item.id}
+                        lazy={index > 2 ? true : false}
+                      />
+                    ))}
+                  </ul>
+                  {item.total > 6 ? (
+                    <Link href={`/category/${item.category.slug}`}>
+                      <a>
+                        <div className="mx-4 flex justify-center border border-white/20 p-2 text-xs">
+                          <span className="uppercase">&nbsp;More</span>
+                        </div>
+                      </a>
+                    </Link>
+                  ) : null}
+                </section>
                 {index < 3 ? (
                   <Banner
                     className={`banner rectangle mt-4 xl:hidden`}
@@ -47,7 +49,7 @@ export default function Home({ data, categories }) {
                     responsive="false"
                   />
                 ) : null}
-              </section>
+              </>
             );
           })}
       </div>
