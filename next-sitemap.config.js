09ab00be9e058427bcae23acc207860465900c02
Tai Zeng <zengtai@gmail.com>
2022-08-15 09:06:49 +0800

    修改主域名

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://playgames.mobi",
+  siteUrl: process.env.SITE_URL || "https://www.playgames.mobi",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
