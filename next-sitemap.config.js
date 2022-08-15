fe295d7780d50c281bd765b3a5f684c5b20d7923
Tai Zeng <zengtai@gmail.com>
2022-08-15 08:36:47 +0800

    修改参数用于playgames.mobi，增加sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://playgames.mobi",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
+
+// export default config;
