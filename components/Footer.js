ed1dbf5f4812a36ccae4300241e99ad3be1a01fd
Tai Zeng <zengtai@gmail.com>
2022-09-08 17:20:34 +0800

    底部添加LOGO

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { SITE_META } from "../lib/constants";
 
 export default function Footer() {
@@ -8,6 +9,16 @@ export default function Footer() {
           <a href={`/t/privacy-policy`}>Privacy Policy</a>
           <a href={`/t/terms-of-use`}>Terms of Use</a>
         </nav>
+        <div className="my-3 w-32">
+          <Image
+            className="h-14 w-auto"
+            src={`/playgames-logo.png`}
+            layout="responsive"
+            alt={SITE_META.name}
+            width={257}
+            height={80}
+          />
+        </div>
         <p>
           {`© ${new Date().getFullYear()} ${SITE_META.name}. All
           Rights Reserved.`}
