0f15ec6e2cb4c5971055684f4baf3cc11bac1dc9
Tai Zeng <zengtai@gmail.com>
2022-11-07 17:16:16 +0800

    修改导出路径

--- next.config.js
+++ next.config.js
@@ -38,13 +38,13 @@ const nextConfig = {
   },
   trailingSlash: true,
   // assetPrefix: `./`,
-  // basePath: "/gamebox", // 20221018
+  basePath: "/gameboxm1", // 20221107
   // basePath: "/webs_test/20221018/gostarfavor", // 20221018
   // distDir: "build",
   generateBuildId: async () => {
-    // You can, for example, get the latest git commit hash here
+    // You can, for example, get the latest git commit hash heren
     // return "20221018";
-    return "20221025";
+    return "20221107";
   },
   async headers() {
     return [
