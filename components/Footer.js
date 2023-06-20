cfe124a6fb110471c4c487d547cfedb8df693834
Tai Zeng <zengtai@gmail.com>
2023-06-20 13:48:18 +0800

    底部添加联系方式

--- components/Footer.js
+++ components/Footer.js
@@ -21,10 +21,34 @@ export default function Footer() {
             height={80}
           />
         </div> */}
-        <p>
-          {`© ${new Date().getFullYear()} ${SITE_META.name}. All
-          Rights Reserved.`}
-        </p>
+        <p
+          className="text-center"
+          dangerouslySetInnerHTML={{
+            __html: `© ${new Date().getFullYear()} ${SITE_META.name} <br >All
+          Rights Reserved`,
+          }}
+        />
+        <div className="my-6 flex items-center justify-center gap-x-1">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+              />
+            </svg>
+          </span>
+          <a className="underline" href="mailto:contact@gostarfavor.com">
+            contact@gostarfavor.com
+          </a>
+        </div>
       </footer>
     </>
   );
