73a0b1758768a7362d2148c46ab583da63e3a90d
Tai Zeng <zengtai@gmail.com>
2023-10-12 16:20:38 +0800

    修改buildid

--- next.config.js
+++ next.config.js
@@ -46,7 +46,7 @@ const nextConfig = {
     // You can, for example, get the latest git commit hash heren
     // return "20221018";
     // return "20221216";
-    return "20231010";
+    return "20231012";
   },
 };
 
